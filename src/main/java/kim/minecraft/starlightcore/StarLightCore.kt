85fe16254ce63eac015e68276917c76783095d1d
贺兰星辰 <shaokeyibb@126.com>
2020-11-26 13:33:29 +0800

    初始化项目 & features.BreakableBucket

---
+++ src/main/java/kim/minecraft/starlightcore/StarLightCore.kt
@@ -0,0 +1,62 @@
+package kim.minecraft.starlightcore
+
+import io.izzel.taboolib.loader.Plugin
+import io.izzel.taboolib.loader.PluginBase
+import io.izzel.taboolib.module.config.TConfig
+import kim.minecraft.starlightcore.features.BreakableBucket
+import org.bukkit.Bukkit
+import org.bukkit.event.HandlerList
+
+object StarLightCore : Plugin() {
+
+    private val config: TConfig by lazy {
+        TConfig.create(plugin, "config.yml")
+    }
+
+    private val message: TConfig by lazy {
+        TConfig.create(plugin, "message.yml")
+    }
+
+    fun Any.getPlugin(): PluginBase = plugin
+
+    fun Any.getConfig(): TConfig = config
+
+    fun Any.getLocale(key: String): String = message.getStringColored(key, "lost locale for $key in message.yml")
+
+    fun Any.getLocaleList(key: String): List<String> = message.getStringListColored(key)
+
+    fun Any.info(msg: String) = plugin.logger.info(msg)
+
+    fun Any.getAdminPermission(): String = config.getString("Global.AdminPermission", "starlightcore.admin")!!
+
+    private fun isFeatureEnable(featureName: String) = config.getBoolean("Features.$featureName.Enable", false)
+
+    override fun onLoad() {
+        // override onLoad()
+    }
+
+    override fun onEnable() {
+        releaseResource()
+        registerEvents()
+    }
+
+    private fun releaseResource() {
+        if (plugin.getResource("config.yml") == null) {
+            info("释放不存在的配置文件 config.yml")
+            plugin.saveResource("config.yml", false)
+        }
+        if (plugin.getResource("message.yml") == null) {
+            info("释放不存在的配置文件 message.yml")
+            plugin.saveResource("message.yml", false)
+        }
+    }
+
+    private fun registerEvents() {
+        if (isFeatureEnable("BreakableBucket"))
+            Bukkit.getPluginManager().registerEvents(BreakableBucket, plugin)
+    }
+
+    override fun onDisable() {
+        HandlerList.unregisterAll(plugin)
+    }
+}
---
+++ src/main/java/kim/minecraft/starlightcore/features/BreakableBucket.kt
@@ -0,0 +1,38 @@
+package kim.minecraft.starlightcore.features
+
+import kim.minecraft.starlightcore.StarLightCore.getConfig
+import kim.minecraft.starlightcore.StarLightCore.getLocale
+import org.bukkit.Material
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerBucketEvent
+import org.bukkit.inventory.ItemStack
+import kotlin.random.Random
+
+object BreakableBucket : Listener {
+
+    private val random = getConfig().getDouble("Features.BreakableBucket.Chance")
+
+    @EventHandler
+    fun onUseBucket(e: PlayerBucketEvent) {
+        if (Random.nextDouble() <= random) {
+            e.player.sendMessage(getLocale("Features.BreakableBucket.Oops"))
+            when (e.bucket) {
+                Material.BUCKET -> {
+                    e.itemStack = ItemStack(Material.IRON_INGOT, 1)
+                    e.isCancelled = true
+                }
+                Material.LAVA_BUCKET -> {
+                    e.itemStack = ItemStack(Material.IRON_INGOT, 1)
+                    e.player.location.block.type = Material.LAVA
+                    e.isCancelled = true
+                }
+                else -> {
+                    e.itemStack = ItemStack(Material.IRON_INGOT, 1)
+                    e.player.location.block.type = Material.WATER
+                    e.isCancelled = true
+                }
+            }
+        }
+    }
+}
