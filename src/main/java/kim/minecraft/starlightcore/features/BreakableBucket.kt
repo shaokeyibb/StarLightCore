18318aed2d01f3fd4236cffe14077ac86aff377f
贺兰星辰 <shaokeyibb@126.com>
2020-11-26 23:39:22 +0800

    BUG修正

--- src/main/java/kim/minecraft/starlightcore/features/BreakableBucket.kt
+++ src/main/java/kim/minecraft/starlightcore/features/BreakableBucket.kt
@@ -2,36 +2,31 @@ package kim.minecraft.starlightcore.features
 
 import kim.minecraft.starlightcore.StarLightCore.getConfig
 import kim.minecraft.starlightcore.StarLightCore.getLocale
+import kim.minecraft.starlightcore.utils.WorldInteraction.isUnderTree
 import org.bukkit.Material
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
-import org.bukkit.event.player.PlayerBucketEvent
+import org.bukkit.event.player.PlayerBucketFillEvent
+import org.bukkit.event.player.PlayerMoveEvent
 import org.bukkit.inventory.ItemStack
 import kotlin.random.Random
 
 object BreakableBucket : Listener {
 
-    private val chance = getConfig().getDouble("Features.BreakableBucket.Chance",0.3)
+    private val chance = getConfig().getDouble("Features.BreakableBucket.Chance", 0.3)
 
     @EventHandler
-    fun onUseBucket(e: PlayerBucketEvent) {
+    fun onFillBucket(e: PlayerBucketFillEvent) {
         if (Random.nextDouble() <= chance) {
             e.player.sendMessage(getLocale("Features.BreakableBucket.Oops"))
-            when (e.bucket) {
-                Material.BUCKET -> {
-                    e.itemStack = ItemStack(Material.IRON_INGOT, 1)
-                    e.isCancelled = true
-                }
-                Material.LAVA_BUCKET -> {
-                    e.itemStack = ItemStack(Material.IRON_INGOT, 1)
+            if (e.blockClicked.type == Material.LAVA) {
+                e.itemStack = ItemStack(Material.IRON_INGOT, 1)
+                if (e.player.location.block.type.name.endsWith("AIR"))
                     e.player.location.block.type = Material.LAVA
-                    e.isCancelled = true
-                }
-                else -> {
-                    e.itemStack = ItemStack(Material.IRON_INGOT, 1)
+            } else if (e.blockClicked.type == Material.WATER) {
+                e.itemStack = ItemStack(Material.IRON_INGOT, 1)
+                if (e.player.location.block.type.name.endsWith("AIR"))
                     e.player.location.block.type = Material.WATER
-                    e.isCancelled = true
-                }
             }
         }
     }
--- src/main/java/kim/minecraft/starlightcore/utils/WorldInteraction.kt
+++ src/main/java/kim/minecraft/starlightcore/utils/WorldInteraction.kt
@@ -10,30 +10,35 @@ object WorldInteraction {
         val maxDistanceBetweenLogAndLeaves = 5
         val treeLeavesNumberAtLeast = 6
         val treeLogUpTo = 1
-        val treeLogDownTo = 3
+        val treeLogDownTo = 1
 
         val highestBlock = world.getHighestBlockAt(location)
 
         if (highestBlock.y <= location.y) return false
         if (!highestBlock.type.name.endsWith("LEAVES")) return false
 
-        val blockList = highestBlock.location.get2DBlocksAround(maxDistanceBetweenLogAndLeaves)
-        if (blockList.filter { it.type.name.endsWith("LOG") }.count() < treeLeavesNumberAtLeast) return false
+        val blockList = highestBlock.location.get3DBlocksAround(maxDistanceBetweenLogAndLeaves)
+        if (!blockList.stream().anyMatch { it.type.name.endsWith("LOG") }) return false
+        if (blockList.filter { it.type.name.endsWith("LEAVES") }.count() < treeLeavesNumberAtLeast) return false
 
         val log = blockList.find { it.type.name.endsWith("LOG") }!!
-        if (!log.location.getYBlocks(treeLogUpTo, treeLogDownTo).all { it.type == log.type }) return false
+        if (!log.location.getYBlocks(treeLogUpTo, treeLogDownTo).all { it.type.name.endsWith("LEAVES") || it.type == log.type }) return false
 
         return true
     }
 
 
-    fun Location.get2DBlocksAround(radius: Int): List<Block> {
+    fun Location.get3DBlocksAround(radius: Int): List<Block> {
         val blockList = mutableListOf<Block>()
-        val max = add(radius.toDouble(), 0.0, radius.toDouble())
-        val min = subtract(radius.toDouble(), 0.0, radius.toDouble())
+        val max = clone().add(radius.toDouble(), radius.toDouble(), radius.toDouble())
+        val min = clone().subtract(radius.toDouble(), radius.toDouble(), radius.toDouble())
         for (loop_x in min.x.toInt()..max.x.toInt()) {
-            for (loop_z in min.z.toInt()..max.z.toInt()) {
-                blockList.add(Location(world, loop_x.toDouble(), y, loop_z.toDouble()).block)
+            for (loop_y in min.y.toInt()..max.y.toInt()) {
+                for (loop_z in min.z.toInt()..max.z.toInt()) {
+                    val block = Location(world, loop_x.toDouble(), loop_y.toDouble(), loop_z.toDouble()).block
+                    if (!block.type.name.endsWith("AIR"))
+                        blockList.add(block)
+                }
             }
         }
         return blockList
@@ -42,7 +47,9 @@ object WorldInteraction {
     fun Location.getYBlocks(up: Int, down: Int): List<Block> {
         val blockList = mutableListOf<Block>()
         for (loop_y in (-down)..up) {
-            blockList.add(clone().add(0.0, loop_y.toDouble(), 0.0).block)
+            val block = clone().add(0.0, loop_y.toDouble(), 0.0).block
+            if (!block.type.name.endsWith("AIR"))
+                blockList.add(block)
         }
         return blockList
     }
