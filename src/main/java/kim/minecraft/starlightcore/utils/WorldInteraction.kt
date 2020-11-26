1be90b6f6922ca85eaafd9d5258399aa563986a2
贺兰星辰 <shaokeyibb@126.com>
2020-11-26 20:54:00 +0800

    世界交互Util

---
+++ src/main/java/kim/minecraft/starlightcore/utils/WorldInteraction.kt
@@ -0,0 +1,49 @@
+package kim.minecraft.starlightcore.utils
+
+import org.bukkit.Location
+import org.bukkit.block.Block
+import org.bukkit.entity.Player
+
+object WorldInteraction {
+
+    fun Player.isUnderTree(): Boolean {
+        val maxDistanceBetweenLogAndLeaves = 5
+        val treeLeavesNumberAtLeast = 6
+        val treeLogUpTo = 1
+        val treeLogDownTo = 3
+
+        val highestBlock = world.getHighestBlockAt(location)
+
+        if (highestBlock.y <= location.y) return false
+        if (!highestBlock.type.name.endsWith("LEAVES")) return false
+
+        val blockList = highestBlock.location.get2DBlocksAround(maxDistanceBetweenLogAndLeaves)
+        if (blockList.filter { it.type.name.endsWith("LOG") }.count() < treeLeavesNumberAtLeast) return false
+
+        val log = blockList.find { it.type.name.endsWith("LOG") }!!
+        if (!log.location.getYBlocks(treeLogUpTo, treeLogDownTo).all { it.type == log.type }) return false
+
+        return true
+    }
+
+
+    fun Location.get2DBlocksAround(radius: Int): List<Block> {
+        val blockList = mutableListOf<Block>()
+        val max = add(radius.toDouble(), 0.0, radius.toDouble())
+        val min = subtract(radius.toDouble(), 0.0, radius.toDouble())
+        for (loop_x in min.x.toInt()..max.x.toInt()) {
+            for (loop_z in min.z.toInt()..max.z.toInt()) {
+                blockList.add(Location(world, loop_x.toDouble(), y, loop_z.toDouble()).block)
+            }
+        }
+        return blockList
+    }
+
+    fun Location.getYBlocks(up: Int, down: Int): List<Block> {
+        val blockList = mutableListOf<Block>()
+        for (loop_y in (-down)..up) {
+            blockList.add(clone().add(0.0, loop_y.toDouble(), 0.0).block)
+        }
+        return blockList
+    }
+}
