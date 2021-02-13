479e7d2bcda65ff41ba7f7c9bf3fd063efd4976a
HikariLan <shaokeyibb@126.com>
2021-02-13 12:40:30 +0800

    Add message for UnsafeTNT

--- src/main/java/kim/minecraft/starlightcore/features/UnsafeTNT.kt
+++ src/main/java/kim/minecraft/starlightcore/features/UnsafeTNT.kt
@@ -2,6 +2,8 @@ package kim.minecraft.starlightcore.features
 
 import kim.minecraft.starlightcore.StarLightCore
 import kim.minecraft.starlightcore.StarLightCore.getConfig
+import kim.minecraft.starlightcore.StarLightCore.getLocale
+import org.bukkit.Bukkit
 import org.bukkit.Material
 import org.bukkit.entity.EntityType
 import org.bukkit.entity.TNTPrimed
@@ -22,15 +24,12 @@ object UnsafeTNT : Listener {
     fun onExplodeTNT(e: PlayerInteractEvent) {
         if (e.action != Action.RIGHT_CLICK_BLOCK || e.material != Material.FLINT_AND_STEEL || e.clickedBlock?.type != Material.TNT) return
         if (Random.nextDouble() <= misfireChange) {
+            e.player.sendMessage(getLocale("Features.UnsafeTNT.MisfireMessage"))
             e.isCancelled = true
             e.clickedBlock!!.type = Material.AIR
             e.player.world.spawn(e.clickedBlock!!.location, TNTPrimed::class.java).also {
                 it.source = e.player
-                object : BukkitRunnable() {
-                    override fun run() {
-                        it.remove()
-                    }
-                }.runTaskLater(StarLightCore.plugin, (it.fuseTicks - 1).toLong())
+                Bukkit.getScheduler().runTaskLater(StarLightCore.plugin, it::remove, (it.fuseTicks - 1).toLong())
             }
         }
     }
@@ -39,6 +38,7 @@ object UnsafeTNT : Listener {
     fun onBreakTNT(e: BlockBreakEvent) {
         if (e.block.type != Material.TNT) return
         if (Random.nextDouble() <= sabotageChange) {
+            e.player.sendMessage(getLocale("Features.UnsafeTNT.SabotageMessage"))
             e.player.world.spawn(e.block.location, TNTPrimed::class.java).source = e.player
         }
     }
